f4ada0eca5baa14f8778867473ec5ca44eca403f
Артём Коршунов <korshun.herooo@yandex.ru>
2021-10-05 18:31:26 +0300

    V.2. 1. Добавлен скрипт БД. 2. Изменён способ подгрузки null картинок.

---
+++ Korshunov_Agents/Model/Agent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korshunov_Agents
+{
+    public partial class Agent
+    {
+        public string CorrectLogo 
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(Logo) || String.IsNullOrWhiteSpace(Logo))
+                {
+                    return @"\Agents\picture.png";
+                }
+                else
+                {
+                    return Logo;
+                }    
+            }
+        }
+    }
+}
